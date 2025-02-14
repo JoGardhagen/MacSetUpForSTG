6166c7ad73d9c4acf76c4bb4dc09facf0e86ee08
Joakim <superapa.spanx@gmail.com>
2025-02-14 12:35:47 +0100

    första commit

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Kontrollera om Homebrew är installerat
+if ! command -v brew &> /dev/null
+then
+    echo "Homebrew är inte installerat. Installerar..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew är redan installerat!"
+fi
+
+# Kontrollera om Homebrew är i PATH
+if ! grep -q "/opt/homebrew/bin" ~/.zprofile
+then
+    echo "Homebrew finns inte i PATH, lägger till det..."
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    echo 'export PATH="/opt/homebrew/bin:$PATH"' >> ~/.zshrc
+    echo "Homebrew har lagts till i din PATH."
+else
+    echo "Homebrew finns redan i din PATH."
+fi
+
+# Ladda om .zprofile och .zshrc för att ändringarna ska träda i kraft
+source ~/.zprofile
+source ~/.zshrc
+
