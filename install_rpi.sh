3592144830efb0e6a483902ab66d246336a2f9f7
Joakim <superapa.spanx@gmail.com>
2025-02-25 18:51:04 +0100

    1

---
+++ install_rpi.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#Updatera paketlistan
+
+echo "Uppdatera paketlistan"
+sudo apt update && sudo apt upgrade -y
+
+install_if_missing(){
+    if ! command -v "$1" &> /dev/null; then
+        echo "Installerar $s..."
+        sudo apt install -y "$2"
+    else
+        echo "$2 redan installerat!"
+    fi
+}
+
+install_if_missing clang++ clang   
+install_if_missing cmake cmake
+
+if ! command -v code &> /dev/null; then
+
+    echo "Installerar VsCode..."      
+
+    if [! -f "/usr/share/keyring/package.microsoft.gpg"]; then
+        echo "Lägger till Microsoft repository för VsCode..."
+        wget -qO- https://package.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/packages.microsoft.gpg > /dev/null
+        echo "deb [arch=arm64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/ code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list> /dev/null
+        sudo apt update
+    fi
+        sudo apt install -y code
+
+else
+    echo "VsCode är redan installerat!"
+fi
+
+ 
+
+if ! code --list-extensions | grep -q "ms-vscode.cpptools-extension-pack"; then
+    echo "Installerar VsCode C++ Extension Pack..."
+    sudo code --install-extension ms-vscode.cpptools-extension-pack --force --user-data-dir="$USER_HOME/.vscode-data"
+else
+    echo "VsCode C++ Tilläggen är redan installerade"
+fi
+
+echo " Installationen klar! "
